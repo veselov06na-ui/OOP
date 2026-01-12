a94fb162198547c8510763500c0c2a0b29fe48c1
Никита <veselov06na@gmail.com>
2026-01-13 01:07:29 +0400

    Создать интерфейс Insertable

---
+++ src/functions/Insertable.java
@@ -0,0 +1,8 @@
+package functions;
+
+/**
+ * Represents a tabulated function that supports inserting or replacing a point.
+ */
+public interface Insertable {
+    void insert(double x, double y);
+}
