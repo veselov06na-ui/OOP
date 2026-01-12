88cc5638ccaf93f2b6c70f99331e9e41ea4d7540
Никита <veselov06na@gmail.com>
2026-01-13 00:01:17 +0400

    Создал интерфейс MathFunction, содержащий метод apply(double x).

--- src/functions/MathFunction.java
+++ src/functions/MathFunction.java
@@ -1,7 +1,7 @@
 package functions;
 
 /**
- * Represents a mathematical function that maps a single double input to a double output.
+ * Represents a mathematical function that maps a single double input to a double output..
  */
 public interface MathFunction {
     double apply(double x);
